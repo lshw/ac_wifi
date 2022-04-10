73e8b99eea51bd2c8b7e3ca83165684aed004491
liu shiwei <liushiwei@gmail.com>
2022-04-11 00:58:46 +0800

    整理save_nvram函数

--- ac_wifi/nvram.h
+++ ac_wifi/nvram.h
@@ -1,21 +1,34 @@
 #ifndef __NVRAM__
 #define __NVRAM__
 
-#define NVRAM7_CHARGE 0b1
 #define NVRAM7_URL    0b10
 #define NVRAM7_UPDATE 0b1000
 
+#define NVRAM_CHARGE 0b1
+#define SET_FILE_CHARGE 0b10
+
+uint8_t set_modi = 0; //不等于0, 需要保存
+uint8_t set_file_modi = 0; //不等于0, 需要保存
 void fix_ac_set();
 struct {
+  uint8_t nc;
   uint8_t nvram7;
   uint8_t change;
-  uint32_t boot_count;
   uint8_t ch;
+  uint32_t boot_count;
   double  kwh; //总度数
   uint32_t  ac_pf; //未换算成度数的pf计数,  超过 sets.ac_kwh_count  就进1到ac_kwh
   uint32_t  ac_pf0; //已统计的HLW8032的pf 如果实际的小于这个数据， 就要把它加上8032的读数， 加到 sets.ac_pf, 并且设置本行为8032读数。
   uint32_t crc32;
-} nvram;
+} __attribute__ ((packed)) nvram;
+
+struct { //不会经常变化的设置， 需要保存到文件系统 sets.dat
+  uint16_t ac_power_change_value;
+  uint16_t ac_voltage_change_value;
+  uint16_t ac_alert_minute;
+  float ac_v_calibration;
+  float ac_i_calibration;
+} __attribute__ ((packed)) sets; //字节紧凑格式， 不做字对齐
 
 uint32_t calculateCRC32(const uint8_t *data, size_t length);
 
