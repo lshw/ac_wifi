14f40d7b0aa21c3c19658780b211a3453a577d78
liu shiwei <liushiwei@gmail.com>
2022-04-26 23:57:44 +0800

    play不需要延迟执行

--- ac_wifi/gpio.h
+++ ac_wifi/gpio.h
@@ -4,20 +4,18 @@
 uint32_t last_check_connected;
 bool last_keygen = HIGH ;
 uint32_t keydown_ms = 0;
-uint8_t ssr_change = 0;
 void key_check() {
   if (last_keygen != digitalRead(KEYWORD)) {
     last_keygen = digitalRead(KEYWORD);
     if (last_keygen == LOW) {
       if (keydown_ms + 20 > millis()) return;
       keydown_ms = millis();
-      ssr_change |= 0x80;
       if (digitalRead(SSR) == HIGH) {
-        ssr_change &= ~1;
         digitalWrite(SSR, LOW);
+        play("321");
       } else {
-        ssr_change |= 1;
         digitalWrite(SSR, HIGH);
+        play("123");
       }
     }
   }
